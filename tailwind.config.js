2bd7268b774166d73bdc2028f66c3b17c4a680bb
KattoDev <alexisrincon27@outlook.com>
2024-12-24 14:45:12 -0500

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ export default {
   content: ['./src/**/*.vue'],
   prefix: 'tw-',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        alert: 'var(--alert)',
+        text: 'var(--text)',
+        background: 'var(--background)',
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        accent: 'var(--accent)',
+      },
+      backgroundImage: {
+        space: "url('/src/assets/images/space.gif')",
+      },
+    },
   },
   plugins: [],
 }
